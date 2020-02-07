aa4f4d997d3bc777e116eb6b4312f1960ab3d355
rdpulgarin <analista.desarrollo@hyundailatinoamerica.com>
2020-02-07 11:04:46 -0500

    avance completar pedido

--- paginas/funcionario_compra/compra_completar.php
+++ paginas/funcionario_compra/compra_completar.php
@@ -48,14 +48,14 @@
 </head>
 <body>
 	<h3><?php echo($titulo_pantalla); ?></h3>
-	<form id="form_revisar_precio">
+	<form id="form_completar_pedido">
 	<div id="contenido_compra_detalle">
 		
 	</div>	
 	<div>
 		<br>
-		<button type="button" id="submit_form_revisar_precio" name="submit_form_revisar_precio" class="btn btn-primary float float-right" name="action">Enviar <i class="fas fa-arrow-right" style="font-size: 18px;"></i></button>	
-		<input type="hidden" name="ejecutar_accion" id="ejecutar_accion" value="funcionario_compra_revisar_precio_update" />
+		<button type="button" id="submit_form_completar_pedido" name="submit_form_completar_pedido" class="btn btn-primary float float-right" name="action">Enviar <i class="fas fa-arrow-right" style="font-size: 18px;"></i></button>	
+		<input type="hidden" name="ejecutar_accion" id="ejecutar_accion" value="funcionario_compra_completar_pedido_update" />
 		<input type="hidden" name="funco_id" id="funco_id" value="<?php echo($funco_id); ?>"/>
 		<input type="hidden" name="fun_id" id="fun_id" value="<?php echo($fun_id); ?>" />
 	</div>
@@ -84,27 +84,34 @@
 		
 		
 		
-		$(document).on('click','.aprobar_mismo_precio',function(){
+		$(document).on('click','.funco_det_forma_pago',function(){
 			var funco_det_id=$(this).attr('funco_det_id');
-			
-			if($(this).is(':checked')){
-				var precio_unitario=$(this).val();
-				$('#funco_det_precio_unitario_aprobado_'+funco_det_id).val(precio_unitario);
-				$('#funco_det_precio_unitario_aprobado_'+funco_det_id).attr('readonly',true);
+			var total=$(this).attr('total');
+			var forma_pago=$(this).val();
+
+			if(forma_pago=='credito'){
+				$('#funco_det_anticipo_'+funco_det_id).attr('readonly',false);
+				$('#funco_det_cuotas_'+funco_det_id).attr('readonly',false);
+				
 			}else{
-				$('#funco_det_precio_unitario_aprobado_'+funco_det_id).val(0);
-				$('#funco_det_precio_unitario_aprobado_'+funco_det_id).attr('readonly',false);
+
+				$('#funco_det_anticipo_'+funco_det_id).attr('readonly',true);
+				$('#funco_det_anticipo_'+funco_det_id).val(total);
+				$('#funco_det_cuotas_'+funco_det_id).attr('readonly',true);
+				$('#funco_det_cuotas_'+funco_det_id).val(1);
+				
 			}
+
 			
 		});
 		
 		
 		//SUBMIT FORM
-		$("#form_revisar_precio").validate();
-		$('#submit_form_revisar_precio').click(function(){
+		$("#form_completar_pedido").validate();
+		$('#submit_form_completar_pedido').click(function(){
 	
-			if($("#form_revisar_precio").valid()){
-				var formData = new FormData(document.getElementById("form_revisar_precio"));	
+			if($("#form_completar_pedido").valid()){
+				var formData = new FormData(document.getElementById("form_completar_pedido"));	
 				$.ajax({
 					type:'POST',
 					dataType: 'json',
