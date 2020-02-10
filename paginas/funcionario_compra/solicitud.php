ac1a4f9ebf83cf6e581be7646e6d979161154295
rdpulgarin <analista.desarrollo@hyundailatinoamerica.com>
2020-02-10 17:30:36 -0500

    bloquea modal para que solo salgan al dar click en la x, a su vez al dar click en la x del modal producto entonces resetea selecet de linea producto

--- paginas/funcionario_compra/solicitud.php
+++ paginas/funcionario_compra/solicitud.php
@@ -227,12 +227,13 @@
 		<br>
 
 	</div>
-	<div class="modal" tabindex="-1" role="dialog" id="modal_lista_productos" >
+	<div class="modal" tabindex="-1" role="dialog" id="modal_lista_productos" data-backdrop="static" 
+  data-keyboard="false">
 	  <div class="modal-dialog modal-lg" role="document">
 	    <div class="modal-content">
 	      <div class="modal-header">
 	        <h5 class="modal-title">Seleccionar Producto</h5>
-	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	        <button type="button" class="close close_modal" data-dismiss="modal" aria-label="Close">
 	          <span aria-hidden="true">&times;</span>
 	        </button>
 	      </div>
@@ -497,6 +498,12 @@
 			
 		});
 		
+		
+		//cerrar modal productos
+		$(document).on('click','.close_modal',function(){
+			$('#producto_receptor').val('');
+			$('#select_producto_linea').val('');			
+		});
 		
 		//eliminar item carrito
 		$(document).on('click','.item_producto_carrito_eliminar',function(){
