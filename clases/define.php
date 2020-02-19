17f8e5e8129c640aded2708fce93355869f97872
Anthony Smidh Urrego Pineda <anthourrego@gmail.com>
2020-02-19 17:42:31 -0500

    Se cambian los parametros del define

--- clases/define.php
+++ clases/define.php
@@ -3,9 +3,9 @@
  date_default_timezone_set('America/Bogota');
 
  //Esta es la ruta disco desde la raiz del servidor hasta donde se encuentran los scripts
- define("RUTA_RAIZ","/intranet_anthony/"); 
+ define("RUTA_RAIZ","/intranet/"); 
 
  //Se define la ruta del servidor donde se hacen las consultas
- define("RUTA_SERVER","/ceg_anthony"); 
+ define("RUTA_SERVER","/ceg/"); 
 
 ?>
