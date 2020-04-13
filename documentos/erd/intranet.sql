dfb318b9278e9b828412808ab5260aed9903c5d0
Anthony Urrego <anthourrego@gmail.com>
2020-04-13 11:11:44 -0500

    Cambios en base de datos

--- documentos/erd/intranet.sql
+++ documentos/erd/intranet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-04-2020 a las 23:38:32
+-- Tiempo de generación: 13-04-2020 a las 18:10:21
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.3
 
@@ -31,6 +31,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `archivos_pi_referencias` (
   `id` int(11) NOT NULL,
   `tipo` mediumtext NOT NULL DEFAULT '',
+  `tipo2` mediumtext NOT NULL DEFAULT '',
   `ruta` mediumtext NOT NULL DEFAULT '',
   `observaciones` mediumtext DEFAULT NULL,
   `fecha_creacion` datetime NOT NULL,
@@ -55,8 +56,19 @@ CREATE TABLE `categorias` (
   `activo` int(11) NOT NULL DEFAULT 1,
   `publico` int(11) NOT NULL DEFAULT 1,
   `fk_categoria` int(11) NOT NULL DEFAULT 0,
-  `fk_creador` int(11) NOT NULL DEFAULT 0
+  `fk_creador` int(11) NOT NULL DEFAULT 0,
+  `fk_permiso` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`, `fecha_creacion`, `aplica_pi`, `activo`, `publico`, `fk_categoria`, `fk_creador`, `fk_permiso`) VALUES
+(1, 'Ficha Técnica', '2020-04-09 18:52:06', 0, 1, 1, 0, 98, 'jobs_cat_ficha_tecnica'),
+(2, 'Fotos en alta', '2020-04-09 18:52:59', 0, 1, 0, 0, 98, 'jobs_cat_fotos_en_alta'),
+(3, 'Fotos Comercial', '2020-04-09 18:53:16', 0, 1, 1, 0, 98, 'jobs_cat_fotos_comercial'),
+(4, 'QA', '2020-04-10 11:07:02', 1, 1, 1, 0, 98, 'jobs_cat_qa');
 
 -- --------------------------------------------------------
 
@@ -201,6 +213,50 @@ CREATE TABLE `tecnologia_no_compatible` (
   `fk_creador` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `tecnologia_no_compatible`
+--
+
+INSERT INTO `tecnologia_no_compatible` (`id`, `fk_tecnologia`, `fk_tecnologia_compatible`, `fecha_creacion`, `estado`, `fk_creador`) VALUES
+(5, 6, 7, '2020-04-09 04:11:11', 1, 98),
+(6, 6, 9, '2020-04-09 04:11:37', 0, 98),
+(7, 6, 8, '2020-04-09 04:12:49', 1, 98),
+(8, 6, 10, '2020-04-09 04:13:02', 0, 98),
+(9, 7, 8, '2020-04-09 04:15:24', 1, 98),
+(10, 7, 9, '2020-04-09 04:15:24', 1, 98),
+(11, 7, 10, '2020-04-09 04:15:24', 1, 98),
+(12, 1, 2, '2020-04-09 10:13:20', 1, 98),
+(13, 3, 4, '2020-04-09 10:14:55', 1, 98),
+(14, 3, 5, '2020-04-09 10:14:55', 1, 98),
+(15, 7, 36, '2020-04-09 13:29:24', 1, 98),
+(16, 10, 36, '2020-04-09 13:29:50', 1, 98),
+(17, 6, 36, '2020-04-09 13:35:50', 0, 98),
+(18, 37, 38, '2020-04-09 14:15:29', 1, 98),
+(19, 42, 37, '2020-04-09 14:15:34', 1, 98),
+(20, 42, 38, '2020-04-09 14:15:34', 1, 98),
+(21, 45, 8, '2020-04-09 14:57:52', 1, 98),
+(22, 46, 45, '2020-04-09 14:58:08', 1, 98),
+(23, 47, 8, '2020-04-09 14:58:46', 1, 98),
+(24, 47, 10, '2020-04-09 14:58:46', 1, 98),
+(25, 47, 45, '2020-04-09 14:58:46', 1, 98),
+(26, 47, 46, '2020-04-09 14:58:46', 1, 98),
+(27, 48, 47, '2020-04-09 14:59:02', 1, 98),
+(28, 49, 1, '2020-04-09 17:04:07', 1, 98),
+(29, 49, 2, '2020-04-09 17:04:07', 1, 98),
+(30, 49, 49, '2020-04-09 17:04:07', 1, 98),
+(31, 49, 19, '2020-04-09 17:04:48', 1, 98),
+(32, 49, 20, '2020-04-09 17:04:48', 1, 98),
+(33, 49, 21, '2020-04-09 17:04:48', 1, 98),
+(34, 49, 22, '2020-04-09 17:04:48', 1, 98),
+(35, 6, 1, '2020-04-09 17:24:58', 1, 98),
+(36, 6, 2, '2020-04-09 17:24:58', 1, 98),
+(37, 7, 1, '2020-04-09 17:25:52', 1, 98),
+(38, 7, 2, '2020-04-09 17:25:52', 1, 98),
+(39, 50, 6, '2020-04-12 19:00:39', 1, 98),
+(40, 50, 7, '2020-04-12 19:00:39', 1, 98),
+(41, 50, 8, '2020-04-12 19:00:39', 1, 98),
+(42, 50, 10, '2020-04-12 19:00:39', 1, 98);
+
 -- --------------------------------------------------------
 
 --
@@ -248,6 +304,22 @@ CREATE TABLE `tipo_archivo_categoria` (
   `fecha_creacion` datetime NOT NULL,
   `fk_creador` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tipo_archivo_categoria`
+--
+
+INSERT INTO `tipo_archivo_categoria` (`id`, `fk_categoria`, `fk_tarchivo`, `estado`, `fecha_creacion`, `fk_creador`) VALUES
+(20, 1, 11, 1, '2020-04-12 18:34:16', 98),
+(21, 2, 1, 1, '2020-04-12 18:36:02', 98),
+(22, 2, 2, 1, '2020-04-12 18:36:06', 98),
+(23, 2, 3, 1, '2020-04-12 18:36:11', 98),
+(24, 3, 1, 1, '2020-04-12 18:36:30', 98),
+(25, 3, 2, 1, '2020-04-12 18:36:36', 98),
+(26, 3, 3, 1, '2020-04-12 18:36:42', 98),
+(27, 4, 5, 1, '2020-04-12 18:37:28', 98),
+(28, 4, 8, 1, '2020-04-12 18:37:55', 98),
+(29, 1, 1, 1, '0000-00-00 00:00:00', 98);
 
 --
 -- Índices para tablas volcadas
@@ -333,7 +405,7 @@ ALTER TABLE `archivos_pi_referencias`
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `logs_jobs`
@@ -375,7 +447,7 @@ ALTER TABLE `tecnologias`
 -- AUTO_INCREMENT de la tabla `tecnologia_no_compatible`
 --
 ALTER TABLE `tecnologia_no_compatible`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT de la tabla `tipo_archivo`
@@ -387,7 +459,7 @@ ALTER TABLE `tipo_archivo`
 -- AUTO_INCREMENT de la tabla `tipo_archivo_categoria`
 --
 ALTER TABLE `tipo_archivo_categoria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
